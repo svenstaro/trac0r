60f8506780eade0a241a608c2dee5cd457fd7ebf
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-10-16 21:37:25 +0200

    Tidy up

--- trac0r/triangle.hpp
+++ trac0r/triangle.hpp
@@ -5,9 +5,10 @@
 #include <glm/gtx/normal.hpp>
 
 struct Triangle {
-    Triangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, glm::vec3 color, glm::vec3 reflectance) : m_v1(v1), m_v2(v2), m_v3(v3), m_color(color), m_reflectance(reflectance) {
+    Triangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3, glm::vec3 emittance, glm::vec3 reflectance)
+        : m_v1(v1), m_v2(v2), m_v3(v3), m_emittance(emittance), m_reflectance(reflectance) {
         m_normal = glm::triangleNormal(v1, v2, v3);
-        
+
         m_centroid.x = v1.x + v2.x + v3.x;
         m_centroid.y = v1.y + v2.y + v3.y;
         m_centroid.z = v1.z + v2.z + v3.z;
@@ -16,7 +17,7 @@ struct Triangle {
     glm::vec3 m_v1;
     glm::vec3 m_v2;
     glm::vec3 m_v3;
-    glm::vec3 m_color;
+    glm::vec3 m_emittance;
     glm::vec3 m_reflectance;
     glm::vec3 m_normal;
     glm::vec3 m_centroid;
--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -39,10 +39,12 @@ int Viewer::init() {
         return 1;
     }
 
-    // m_render = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    // m_render = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED |
+    // SDL_RENDERER_PRESENTVSYNC);
     m_render = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
-    m_render_tex = SDL_CreateTexture(m_render, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, width, height);
-    m_pixels.resize(width*height, 0);
+    m_render_tex = SDL_CreateTexture(m_render, SDL_PIXELFORMAT_ARGB8888,
+                                     SDL_TEXTUREACCESS_STREAMING, width, height);
+    m_pixels.resize(width * height, 0);
 
     if (m_render == nullptr) {
         SDL_DestroyWindow(m_window);
@@ -55,7 +57,7 @@ int Viewer::init() {
         std::cerr << "SDL_ttf could not initialize! SDL_ttf error: " << SDL_GetError() << std::endl;
         return 1;
     }
-    
+
     // Setup scene
     SDL_SetRelativeMouseMode(SDL_TRUE);
     setup_scene();
@@ -66,10 +68,13 @@ int Viewer::init() {
 }
 
 void Viewer::setup_scene() {
-    auto triangle = std::make_unique<Triangle>(glm::vec3{0.f, 5.f, 0.f}, glm::vec3{5.f, 5.f, 0.f}, glm::vec3{5.f, 5.f, 5.f}, glm::vec3{0.8, 0.3, 0.3}, glm::vec3{0.5, 0.5, 0.5});
+    auto triangle = std::make_unique<Triangle>(glm::vec3{0.f, 5.f, 0.f}, glm::vec3{0.5f, 5.f, 0.f},
+                                               glm::vec3{0.5f, 5.f, 0.5f}, glm::vec3{0.3, 0.3, 0.3},
+                                               glm::vec3{0.2, 0.2, 0.2});
     m_scene.push_back(std::move(triangle));
     // for (auto i = 0; i < 2; i++) {
-    //     auto triangle = std::make_unique<Triangle>(glm::ballRand(5.f), glm::ballRand(5.f), glm::ballRand(5.f), glm::vec3{0.8, 0.3, 0.3}, glm::vec3{0.5, 0.5, 0.5});
+    //     auto triangle = std::make_unique<Triangle>(glm::ballRand(5.f), glm::ballRand(5.f),
+    //     glm::ballRand(5.f), glm::vec3{0.8, 0.3, 0.3}, glm::vec3{0.5, 0.5, 0.5});
     //     m_scene.push_back(std::move(triangle));
     // }
 
@@ -90,9 +95,10 @@ glm::vec3 Viewer::intersect_scene(glm::vec3 &ray_pos, glm::vec3 &ray_dir, int de
     // check all triangles for collision
     bool collided = false;
     glm::vec3 ret_color;
-    for (const auto& tri : m_scene) {
+    for (const auto &tri : m_scene) {
         glm::vec3 bary_pos;
-        collided = glm::intersectRayTriangle(ray_pos, ray_dir, tri->m_v1, tri->m_v2, tri->m_v3, bary_pos);
+        collided =
+            glm::intersectRayTriangle(ray_pos, ray_dir, tri->m_v1, tri->m_v2, tri->m_v3, bary_pos);
 
         if (collided) {
             glm::vec3 new_ray_pos = ray_pos + ray_dir * bary_pos.z;
@@ -104,15 +110,14 @@ glm::vec3 Viewer::intersect_scene(glm::vec3 &ray_pos, glm::vec3 &ray_dir, int de
             glm::vec3 brdf = 2.f * tri->m_reflectance * cos_theta;
             glm::vec3 reflected = intersect_scene(new_ray_pos, new_ray_dir, depth + 1);
 
-            ret_color = tri->m_color + (brdf * reflected);
+            ret_color = tri->m_emittance + (brdf * reflected);
             break;
         }
     }
 
     if (collided) {
         return ret_color;
-    }
-    else {
+    } else {
         return {0, 0, 0};
     }
 }
@@ -139,9 +144,9 @@ void Viewer::mainloop() {
         cam_velocity.x += 1;
 
     if (keystates[SDL_SCANCODE_W])
-        cam_velocity.y += -1;
-    else if (keystates[SDL_SCANCODE_S])
         cam_velocity.y += 1;
+    else if (keystates[SDL_SCANCODE_S])
+        cam_velocity.y += -1;
 
     m_camera.pos += cam_velocity;
 
@@ -149,7 +154,7 @@ void Viewer::mainloop() {
     int width;
     int height;
     SDL_GetWindowSize(m_window, &width, &height);
-    auto aspect_ratio = (float)width/(float)height;
+    auto aspect_ratio = (float)width / (float)height;
 
     int mouse_x;
     int mouse_y;
@@ -161,26 +166,34 @@ void Viewer::mainloop() {
     std::cout << glm::to_string(m_camera.pos) << std::endl;
     std::cout << glm::to_string(m_camera.dir) << std::endl;
 
-    glm::mat4 projection_matrix = glm::perspective(m_camera.fov, aspect_ratio, m_camera.near_plane_dist, m_camera.far_plane_dist);
+    glm::mat4 projection_matrix = glm::perspective(
+        m_camera.fov, aspect_ratio, m_camera.near_plane_dist, m_camera.far_plane_dist);
     glm::mat4 view_matrix = glm::lookAt(m_camera.pos, m_camera.pos + m_camera.dir, m_camera.up);
 
     // Sort by distance to camera
-    std::sort(m_scene.begin(), m_scene.end(), [this](const auto &tri1, const auto &tri2){return glm::distance(m_camera.pos, tri1->m_centroid) < glm::distance(m_camera.pos, tri2->m_centroid);});
+    std::sort(m_scene.begin(), m_scene.end(), [this](const auto &tri1, const auto &tri2) {
+        return glm::distance(m_camera.pos, tri1->m_centroid) <
+               glm::distance(m_camera.pos, tri2->m_centroid);
+    });
     for (auto sample_cnt = 0; sample_cnt < 2; sample_cnt++) {
         for (auto x = 0; x < width; x++) {
             for (auto y = 0; y < height; y++) {
                 glm::vec3 win_coords{x, y, 0};
                 glm::mat4 model{1.0f};
                 glm::vec4 viewport{0, 0, width, height};
-                glm::vec3 object_coords = glm::unProject(win_coords, model * view_matrix, projection_matrix, viewport);
+                glm::vec3 object_coords =
+                    glm::unProject(win_coords, model * view_matrix, projection_matrix, viewport);
                 glm::vec3 color = intersect_scene(object_coords, m_camera.dir, 0);
-                // std::cout << "r: " << color.r << " g: " << color.g << " b: " << color.b << std::endl;
-                m_pixels[y * width + x] = 0xff << 24 | int(color.r * 255) << 16 | int(color.g * 255) << 8 | int(color.b * 255);
+                // std::cout << "r: " << color.r << " g: " << color.g << " b: " << color.b <<
+                // std::endl;
+                m_pixels[y * width + x] = 0xff << 24 | int(color.r * 255) << 16 |
+                                          int(color.g * 255) << 8 | int(color.b * 255);
             }
         }
     }
-    
-    SDL_SetRenderDrawColor(m_render, glm::linearRand(0, 255), glm::linearRand(0, 255), glm::linearRand(0, 255), 255);
+
+    SDL_SetRenderDrawColor(m_render, glm::linearRand(0, 255), glm::linearRand(0, 255),
+                           glm::linearRand(0, 255), 255);
     SDL_RenderClear(m_render);
     SDL_UpdateTexture(m_render_tex, 0, m_pixels.data(), width * sizeof(uint32_t));
     SDL_RenderCopy(m_render, m_render_tex, 0, 0);
@@ -192,7 +205,7 @@ void Viewer::mainloop() {
     (void)dt;
     m_last_frame_time = current_time;
 
-    std::cout << "FPS: " << 1./dt << std::endl;
+    std::cout << "FPS: " << 1. / dt << std::endl;
 }
 
 SDL_Renderer *Viewer::renderer() {
