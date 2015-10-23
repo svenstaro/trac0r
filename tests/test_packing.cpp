61a0dfe127e56ec8ecbdbeaae962cc21e470e072
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-10-23 04:04:44 +0200

    More test runs

--- tests/test_packing.cpp
+++ tests/test_packing.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     (void)argv;
 
     auto unpacked = glm::vec4{0.5, 0.5, 0.5, 0.5};
-    for (int i = 0; i < 1; ++i) {
+    for (int i = 0; i < 10000; ++i) {
         auto packed = trac0r::pack_color_argb(unpacked);
         unpacked = trac0r::unpack_color_argb_to_vec4(packed);
     }
