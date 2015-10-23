ebbfcfa773e5d4f572c3427ad5f0dcfd51e51ebf
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-10-23 03:48:33 +0200

    Add minimal tests

---
+++ tests/test_packing.cpp
@@ -0,0 +1,21 @@
+#include "trac0r/utils.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    auto unpacked = glm::vec4{0.5, 0.5, 0.5, 0.5};
+    for (int i = 0; i < 1; ++i) {
+        auto packed = trac0r::pack_color_argb(unpacked);
+        unpacked = trac0r::unpack_color_argb_to_vec4(packed);
+    }
+
+    std::cout << glm::to_string(unpacked) << std::endl;
+
+    return 0;
+}
