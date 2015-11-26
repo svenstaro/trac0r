58f3b3a3f2c95acda32d04895f39f9bfa571cb80
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-11-26 06:03:59 +0100

    Add missing files

---
+++ trac0r/intersection_info.hpp
@@ -0,0 +1,36 @@
+#ifndef INTERSECTION_INFO_HPP
+#define INTERSECTION_INFO_HPP
+
+#include "material.hpp"
+#include "ray.hpp"
+
+#include <glm/glm.hpp>
+
+struct IntersectionInfo {
+    /**
+     * @brief Indicates whether an intersection was found.
+     */
+    bool m_has_intersected = false;
+
+    /**
+     * @brief World coordinates of intersection.
+     */
+    glm::vec3 m_pos;
+
+    /**
+     * @brief Normal at the point of intersection.
+     */
+    glm::vec3 m_normal;
+
+    /**
+     * @brief Direction vector of incomig ray towards point of intersection.
+     */
+    Ray m_incoming_ray = Ray(glm::vec3(0), glm::vec3(0));
+
+    /**
+     * @brief Material at the point of intersection.
+     */
+    Material m_material;
+};
+
+#endif /* end of include guard: INTERSECTION_INFO_HPP */
---
+++ trac0r/material.hpp
@@ -0,0 +1,11 @@
+#ifndef MATERIAL_HPP
+#define MATERIAL_HPP
+
+#include <glm/glm.hpp>
+
+struct Material {
+    glm::vec3 m_reflectance;
+    glm::vec3 m_emittance;
+};
+
+#endif /* end of include guard: MATERIAL_HPP */
