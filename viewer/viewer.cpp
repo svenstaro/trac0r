d0b90e8f6b2ec408a84991c63b1553a24404f8ae
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-12-22 14:13:50 +0100

    Make icosphere refractive

--- viewer/viewer.cpp
+++ viewer/viewer.cpp
@@ -97,7 +97,7 @@ void Viewer::setup_scene() {
     trac0r::Material diffuse_red{{0.366046, 0.0371827, 0.0416385}, {0, 0, 0}, 0, {0, 0, 0}};
     trac0r::Material diffuse_green{{0.162928, 0.408903, 0.0833759}, {0, 0, 0}, 0, {0, 0, 0}};
     trac0r::Material reflective{{0, 0, 0}, {1, 1, 1}, 0, {0, 0, 0}};
-    // trac0r::Material refractive{{0, 0, 0}, {0, 0, 0}, 1.51714f, {0, 0, 0}};
+    trac0r::Material refractive{{0, 0, 0}, {0, 0, 0}, 1.51714f, {0, 0, 0}};
     trac0r::Material emissive{{0, 0, 0}, {0, 0, 0}, 0, {10, 10, 9}};
     auto wall_left = trac0r::Shape::make_plane({-0.5f, 0.4f, 0}, {1, 0, 0}, {1, 1}, diffuse_red);
     auto wall_right = trac0r::Shape::make_plane({0.5f, 0.4f, 0}, {-1, 0, 0}, {1, 1}, diffuse_green);
@@ -112,7 +112,7 @@ void Viewer::setup_scene() {
     auto box2 = trac0r::Shape::make_box({-0.2f, 0.05f, 0.1f}, {0.f, 1.f, 0.f}, {0.3f, 0.4f, 0.3f},
                                         reflective);
     auto sphere1 =
-        trac0r::Shape::make_icosphere({0.f, 0.f, -0.3f}, {0.f, 1.f, 0.f}, 0.15f, 1, default_material);
+        trac0r::Shape::make_icosphere({0.f, 0.1f, -0.3f}, {0.f, 1.f, 0.f}, 0.15f, 1, refractive);
 
     Scene::add_shape(m_scene, wall_left);
     Scene::add_shape(m_scene, wall_right);
